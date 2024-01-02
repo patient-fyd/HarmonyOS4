0afd12a1a39378eea94688ee8b7fb574521140be
GanFengYu <patient.fyd@gmail.com>
2024-01-02 11:46:14 +0800

    贪吃蛇项目搭建

---
+++ typescript/snakeGame/src/index.ts
@@ -0,0 +1,3 @@
+// 引入样式
+import './style/index.less'
+console.log(123);
---
+++ typescript/snakeGame/webpack.config.js
@@ -0,0 +1,114 @@
+// 引入一个包
+const path = require('path');
+// 引入html插件
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+// 引入clean插件
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+
+// webpack中的所以配置信息都应该写在module.exports中
+module.exports = {
+    // 指定构建环境
+    mode: "development",
+    // 指定入口文件
+    entry: "./src/index.ts",
+
+    // 指定打包文件所在目录
+    output: {
+        // 指定打包文件的目录
+        path: path.resolve(__dirname, 'dist'),
+        // 打包文件的文件名
+        filename: 'bundle.js',
+
+        // 告诉webpack不使用箭头函数
+        environment: {
+            arrowFunction: false
+        }
+    },
+
+    devServer: {
+        static: './dist',
+    },
+
+    // 指定webpack打包时要使用的模块
+    module: {
+        // 指定要加载的规则
+        rules: [
+            {
+                // test指定的是规则生效的文件
+                test: /\.ts$/,
+                // 要使用的loader
+                use: [
+                    // 配置babel
+                    {
+                        // 指定加载器
+                        loader: 'babel-loader',
+                        // 设置babel
+                        options: {
+                            // 设置预定义的环境
+                            presets: [
+                                [
+                                    // 指定环境的插件
+                                    '@babel/preset-env',
+                                    // 配置信息
+                                    {
+                                        // 要兼容的目标浏览器
+                                        targets: {
+                                            'chrome': '58',
+                                            'ie': '11'
+                                        },
+                                        // 指定corejs的版本
+                                        'corejs': '3',
+                                        // 使用corejs的方式使用polyfill,usage表示按需加载
+                                        'useBuiltIns': 'usage'
+                                    }
+                                ]
+                            ]
+                        }
+                    },
+                    'ts-loader',
+                ],
+                // 要排除的文件
+                exclude: /node_modules/,
+            },
+
+            // 设置less文件的处理
+            {
+                test: /\.less$/,
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    {
+                        loader: "postcss-loader",
+                        options: {
+                            postcssOptions: {
+                                plugins: [
+                                    [
+                                        "postcss-preset-env",
+                                        {
+                                            browsers: "last 2 versions",
+                                        },
+                                    ],
+                                ],
+                            },
+                        },
+                    },
+                    "less-loader"
+                ]
+            }
+        ]
+    },
+
+    // 配置webpack插件
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            //title: "test"
+            template: "./src/index.html"
+        }),
+    ],
+
+    // 用来设置引用模块
+    resolve: {
+        extensions: ['.ts', '.js'],
+    }
+};
