7ba870245ce61ce44a2458c4da9ff95659f0fde3
GanFengYu <patient.fyd@gmail.com>
2024-01-02 10:58:14 +0800

    属性的封装

---
+++ typescript/part4/src/07属性的封装.ts
@@ -0,0 +1,48 @@
+(function () {
+    // 定义一个表示人的类
+    class Person {
+        /**
+         * TS可以在属性前添加属性的修饰符
+         *      public 修饰的属性可以在任意位置修改
+         *      private 修饰的属性只能在类内部进行访问
+         */
+        private _name: string;
+        private _age: number;
+        constructor(name: string, age: number) {
+            this._name = name;
+            this._age = age;
+        }
+
+
+        get name(): string {
+            return this._name;
+        }
+
+        set name(value: string) {
+            this._name = value;
+        }
+
+        get age(): number {
+            return this._age;
+        }
+
+        set age(value: number) {
+            if (value < 0) {
+                throw new Error("年龄不能为负数");
+            }else {
+                this._age = value;
+            }
+        }
+
+        sayHi() {
+            console.log(`My name is ${this._name}.`);
+        }
+    }
+
+    const per = new Person("张三", 18);
+    per.age = 20;
+    per.name = "李四";
+    console.log(per);
+
+
+})();
