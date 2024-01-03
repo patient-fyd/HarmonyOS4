863317ed2f4e906077806292fd1b61fada081df5
GanFengYu <patient.fyd@gmail.com>
2024-01-03 09:12:39 +0800

    添加键盘控制方向

--- typescript/snakeGame/src/moduls/GameController.ts
+++ typescript/snakeGame/src/moduls/GameController.ts
@@ -11,6 +11,9 @@ class GameController{
     food: Food;
     // 记分牌
     scorePanel: ScorePanel;
+    // 游戏是否结束
+    isLive = true;
+
     constructor() {
         this.snake = new Snake();
         this.food = new Food();
@@ -23,6 +26,7 @@ class GameController{
     init() {
         // 绑定键盘按键按下的事件
         document.addEventListener('keydown', this.keydownHandler.bind(this));
+        this.moveSnake();
     }
 
     // 键盘按下的响应函数
@@ -46,6 +50,38 @@ class GameController{
         }
         // 修改方向
         this.snake.direction = event.key;
+    }
+
+    /**
+     * 控制蛇移动的方法
+     * 根据方向来使蛇的位置改变
+     */
+    moveSnake() {
+        // 获取蛇现在坐标
+        let X = this.snake.X;
+        let Y = this.snake.Y;
+        // 根据当前方向移动蛇的位置
+        switch (this.snake.direction) {
+            case 'ArrowUp' || 'Up' || 'w':
+                Y -= 10;
+                break;
+            case 'ArrowDown' || 'Down' || 's':
+                Y += 10;
+                break;
+            case 'ArrowLeft' || 'Left' || 'a':
+                X -= 10;
+                break;
+            case 'ArrowRight' || 'Right' || 'd':
+                X += 10;
+                break;
+        }
+
+        // 修改蛇的X和Y值
+        this.snake.X = X;
+        this.snake.Y = Y;
+
+        // 开启一个定时调用,随着等级提升速度加快
+        this.isLive && setTimeout(this.moveSnake.bind(this), 300 - (this.scorePanel.level - 1) * 30);
     }
 }
 
